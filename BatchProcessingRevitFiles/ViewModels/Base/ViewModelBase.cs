ac50f3f4134ac79bb560030ab114adbfc65c25ec
Максим <iioxyuct88@mail.ru>
2021-03-22 10:36:40 +0300

    Добавьте файлы проекта.

---
+++ BatchProcessingRevitFiles/ViewModels/Base/ViewModelBase.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace BatchProcessingRevitFiles
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
